6cadac3a97c334368adc5de1fe8ec1592283ffb2
Abkreno <mohamedelhamamsy789@gmail.com>
2017-09-24 14:49:32 +0200

    #1169: Add brython / ace editor js files

---
+++ js/main.js
@@ -0,0 +1,25 @@
+window.writeResultsToConsole = function (executedCode) {
+    window
+        .streamsApi
+        .setUserCodeExecutedThusFar({executedCode});
+}
+
+window.exec = function (code) {
+    try {
+        window._exec(code);
+    } catch (e) {
+        window
+            .streamsApi
+            .setUserCodeExecutedThusFar({
+                executedCode: e.toString(),
+                isError: true
+            });
+    }
+}
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/xcode");
+editor.getSession().setMode("ace/mode/python");
+editor.setOptions({
+    fontSize: '18px'
+});
