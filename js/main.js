c304b47bec661555f49d6e146a46e5d6e42167cb
Abkreno <mohamedelhamamsy789@gmail.com>
2017-09-29 18:08:00 +0200

    #1192: Make editor/console resizable

--- js/main.js
+++ js/main.js
@@ -5,3 +5,26 @@ $('[data-action="run"]').click(function(){
 $('[data-action="clear-console"]').click(function(){
     window.clearConsole();
 });
+
+var container = $(".wrapper");
+var numberOfCol = 2;
+$(".resizable").css('width', 100/numberOfCol +'%');
+
+var sibTotalWidth;
+$(".resizable").resizable({
+    handles: 'e',
+    start: function(event, ui){
+        sibTotalWidth = ui.originalSize.width + ui.originalElement.next().outerWidth();
+    },
+    stop: function(event, ui){     
+        var cellPercentWidth=100 * ui.originalElement.outerWidth()/ container.innerWidth();
+        ui.originalElement.css('width', cellPercentWidth + '%');  
+        var nextCell = ui.originalElement.next();
+        var nextPercentWidth=100 * nextCell.outerWidth()/container.innerWidth();
+        nextCell.css('width', nextPercentWidth + '%');
+        editor.resize();        
+    },
+    resize: function(event, ui){ 
+        ui.originalElement.next().width(sibTotalWidth - ui.size.width); 
+    }
+});
