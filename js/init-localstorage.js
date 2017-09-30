61207b1dddd7785a3fef678f3590d0fc90b8dad3
Abkreno <mohamedelhamamsy789@gmail.com>
2017-09-30 16:31:22 +0200

    #1198: Move events logic to main

--- js/init-localstorage.js
+++ js/init-localstorage.js
@@ -17,17 +17,17 @@ const deleteSession = function(sessionIndex) {
     localStorage.setItem('SESSIONS', JSON.stringify(SESSIONS));
     if (CURRENT_SESSION_INDEX === undefined) {
         return true;
-    } 
+    }
     if (CURRENT_SESSION_INDEX === sessionIndex) {
         editor.setValue('', 1);
         localStorage.removeItem('CURRENT_SESSION_INDEX');
         CURRENT_SESSION_INDEX = undefined;
-        $('#session-title').text('');        
-    } else if(CURRENT_SESSION_INDEX > sessionIndex) {
+        $('#session-title').text('');
+    } else if (CURRENT_SESSION_INDEX > sessionIndex) {
         CURRENT_SESSION_INDEX = CURRENT_SESSION_INDEX - 1;
-        localStorage.setItem('CURRENT_SESSION_INDEX', CURRENT_SESSION_INDEX);       
+        localStorage.setItem('CURRENT_SESSION_INDEX', CURRENT_SESSION_INDEX);
     }
-    loadAllSessions();
+    loadAllSessionsToModal();
 };
 
 
@@ -44,26 +44,26 @@ const handleDeleteAction = function(sessionIndex) {
         confirmButtonClass: 'btn btn-danger',
         cancelButtonClass: 'btn btn-default',
         buttonsStyling: false
-      }).then(function () {
+    }).then(function() {
         deleteSession(sessionIndex);
-      }, function (dismiss) {
+    }, function(dismiss) {
         // dismiss can be 'cancel', 'overlay',
         // 'close', and 'timer'
         return false;
     });
 }
 
-const loadAllSessions = function(sessionIndex) {
+const loadAllSessionsToModal = function(sessionIndex) {
     const SESSIONS = JSON.parse(localStorage.getItem('SESSIONS')) || [];
     $('#sessions-container').empty();
     if (SESSIONS.length) {
-        for(var index = 0; index < SESSIONS.length; index++) {
+        for (var index = 0; index < SESSIONS.length; index++) {
             $('#sessions-container').append(`<div class="card">
             <h4 data-action="load-session" data-id="${index}" class="card-title">${SESSIONS[index].name}</h4>
             <i data-action="delete-session" data-id="${index}" class="fa fa-trash card-icon"></i>
-            </div>`);    
+            </div>`);
         }
-       
+
         $('[data-action="load-session"').click(function(event) {
             let sessionIndex = $(event.currentTarget).attr('data-id') || 0;
             sessionIndex = parseInt(sessionIndex, 10);
@@ -76,21 +76,27 @@ const loadAllSessions = function(sessionIndex) {
             sessionIndex = parseInt(sessionIndex, 10);
             handleDeleteAction(sessionIndex);
         });
-    
+
     } else {
         $('#sessions-container').append('<h4 class="text-center">There are no sessions to load!</h4>');
     }
 }
 
-const generateNewSessionName= function() {
+const generateNewSessionName = function() {
     return 'Session-' + moment().format('MMMM-D-Y-{HH:mm}');
 }
 
-const saveSession = function({sessionName, sessionIndex}) {
+const saveSession = function({
+    sessionName,
+    sessionIndex
+}) {
     let SESSIONS = JSON.parse(localStorage.getItem('SESSIONS')) || [];
     const code = editor.getValue();
     if (sessionName) {
-        SESSIONS.push({code, name: sessionName});
+        SESSIONS.push({
+            code,
+            name: sessionName
+        });
         sessionIndex = SESSIONS.length - 1;
     } else if (sessionIndex >= 0) {
         SESSIONS[sessionIndex].code = code;
@@ -99,33 +105,38 @@ const saveSession = function({sessionName, sessionIndex}) {
             text: SESSIONS[sessionIndex].name,
             type: "success",
             timer: 3000
-            }).then(
-            function () {
+        }).then(
+            function() {
                 return true;
-            }, function() {
+            },
+            function() {
                 return false;
-            });    
+            });
     }
     localStorage.setItem('SESSIONS', JSON.stringify(SESSIONS));
     loadSessionCodeToEditor(sessionIndex);
 }
 
 const handleSaveAction = function(sessionIndex) {
-    if(sessionIndex >= 0) {
-        saveSession({sessionIndex});        
+    if (sessionIndex >= 0) {
+        saveSession({
+            sessionIndex
+        });
     } else {
         swal({
             title: 'Save',
-            text: 'Enter File Name:',                   
+            text: 'Enter File Name:',
             showCancelButton: true,
             input: 'text',
             inputValue: generateNewSessionName(),
             confirmButtonText: 'Save',
             confirmButtonColor: '#4aa0f1',
             cancelButtonColor: '#898b8e',
-          }).then(function (sessionName) {  
-            saveSession({sessionName});
-        }, function (dismiss) {
+        }).then(function(sessionName) {
+            saveSession({
+                sessionName
+            });
+        }, function(dismiss) {
             // dismiss can be 'cancel', 'overlay',
             // 'close', and 'timer'
             return false;
@@ -133,11 +144,31 @@ const handleSaveAction = function(sessionIndex) {
     }
 }
 
-const handleNewAction = function() {
-    editor.setValue('', 1);  
+const newSession = function() {
+    editor.setValue('', 1);
     localStorage.removeItem('CURRENT_SESSION_INDEX');
     CURRENT_SESSION_INDEX = undefined;
     $('#session-title').text('');
+}
+
+const handleNewAction = function() {
+    if (CURRENT_SESSION_INDEX === undefined && editor.getValue().length === 0) {
+        newSession();
+    } else {
+        swal({
+            title: 'Are you sure?',
+            text: 'Please make sure you saved the current session.',
+            showCancelButton: true,
+            confirmButtonText: 'Continue',
+            confirmButtonColor: '#4aa0f1',
+            cancelButtonColor: '#898b8e',
+        }).then(function(sessionName) {
+            newSession();
+        }, function(dismiss) {
+            return false;
+        });
+    }
+
 }
 
 const loadCurrentSessionIndex = function() {
@@ -151,39 +182,19 @@ if (typeof(Storage) !== 'undefined') {
     if (CURRENT_SESSION_INDEX >= 0) {
         loadSessionCodeToEditor(CURRENT_SESSION_INDEX);
     }
-    $('[data-action="save"]').click(function() {
-        handleSaveAction(CURRENT_SESSION_INDEX);
-    });
-    $('[data-action="new"]').click(function() {
-        if(CURRENT_SESSION_INDEX === undefined && editor.getValue().length === 0) {
-            handleNewAction();
-        } else {
-            swal({
-                title: 'Are you sure?',
-                text: 'Please make sure you saved the current session.',                   
-                showCancelButton: true,
-                confirmButtonText: 'Continue',
-                confirmButtonColor: '#4aa0f1',
-                cancelButtonColor: '#898b8e',
-            }).then(function (sessionName) {  
-                handleNewAction();
-            }, function (dismiss) {
-                // dismiss can be 'cancel', 'overlay',
-                // 'close', and 'timer'
-                return false;
-            });        
-        }
-    });
-    $('[data-action="load"').click(function() {
-        loadAllSessions();
-    });
     
+    window.saveSession = () => handleSaveAction(CURRENT_SESSION_INDEX);
+
+    window.newSession = () => handleNewAction();
+
+    window.loadAllSessionsToModal = () => loadAllSessionsToModal();
+
 } else {
     // Sorry! No Web Storage support..
     swal({
         title: 'Sorry!',
         text: 'No Web Storage support..',
         icon: "warning",
-        dangerMode: true,      
+        dangerMode: true,
     });
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
+// EDITOR EVENTS
 $('[data-action="run"]').click(function() {
     window.exec(editor.getValue());
 });
+
+// SESSIONS 
+
+$('[data-action="save"]').click(function() {
+    window.saveSession();
+});
+$('[data-action="new"]').click(function() {
+    window.newSession();
+});
+$('[data-action="load-sessions-to-modal"').click(function() {
+    window.loadAllSessionsToModal();
+});
+
+// CONSOLE EVENTS
 
 $('[data-action="clear-console"]').click(function() {
     window.clearConsole();
